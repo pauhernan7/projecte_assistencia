572a8a7e1f02dd3690bc7c6d485c2971c4c894bd
Achraf Chakir <2023_achraf.chakir@iticbcn.cat>
2024-11-22 16:14:27 +0100

    Main completo, rebre dades de aws, insertar dades a la bdd

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import pymysql
 # Configuración de AWS IoT
 ENDPOINT = "a202irx8mp4wwa-ats.iot.us-east-1.amazonaws.com"  # Tu endpoint MQTT
 CLIENT_ID = "mi_dispositivo"         # Identificador único para tu dispositivo
-TOPIC = "esp32/data"                 # Topic al que te suscribirás
+TOPIC = "rfid/uid"                 # Topic al que te suscribirás
 CERTIFICATE_PATH = "certificats/certificate.pem.crt"
 PRIVATE_KEY_PATH = "certificats/private.pem.key"
 CA_PATH = "certificats/AmazonRootCA1.pem"
@@ -15,8 +15,8 @@ CA_PATH = "certificats/AmazonRootCA1.pem"
 # Configuración de MySQL
 DB_HOST = "localhost"
 DB_USER = "root"
-DB_PASSWORD = "tu_password"
-DB_NAME = "proyecte_gestio_assistencia"
+DB_PASSWORD = "system"
+DB_NAME = "projecte_assistencia"
 
 # Callback para cuando se recibe un mensaje
 def on_message_received(topic, payload, **kwargs):
@@ -29,6 +29,7 @@ def on_message_received(topic, payload, **kwargs):
         print(f"Error procesando el mensaje: {e}")
 
 # Conectar a la base de datos y guardar datos
+# Conectar a la base de datos y guardar datos
 def insertar_en_base_de_datos(data):
     try:
         conexion = pymysql.connect(
@@ -38,13 +39,23 @@ def insertar_en_base_de_datos(data):
             database=DB_NAME
         )
         with conexion.cursor() as cursor:
-            query = """
-                INSERT INTO Asistencias (usuario_id, fecha, hora_entrada, estado)
-                VALUES (%s, %s, %s, %s)
-            """
-            cursor.execute(query, (data['usuario_id'], data['fecha'], data['hora_entrada'], data['estado']))
-            conexion.commit()
-        print("Datos insertados correctamente en la base de datos.")
+            # Comprobar si el UID está asociado a un usuario
+            query_buscar_usuario = "SELECT id FROM Usuarios WHERE uid = %s"
+            cursor.execute(query_buscar_usuario, (data['uid'],))
+            resultado = cursor.fetchone()
+
+            if resultado:
+                usuario_id = resultado[0]  # Extraer el ID del usuario
+                # Insertar un registro en la tabla Asistencias
+                query_insertar_asistencia = """
+                    INSERT INTO Asistencias (usuario_id, fecha, hora_entrada, estado)
+                    VALUES (%s, CURDATE(), CURTIME(), 'presente')
+                """
+                cursor.execute(query_insertar_asistencia, (usuario_id,))
+                conexion.commit()
+                print("Datos insertados correctamente en la base de datos.")
+            else:
+                print(f"No se encontró un usuario con el UID: {data['uid']}")
     except Exception as e:
         print(f"Error insertando en la base de datos: {e}")
     finally:
